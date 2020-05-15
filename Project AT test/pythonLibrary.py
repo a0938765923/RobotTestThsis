01f227d57ed89c16f5e55d5293d08eacebe6d81f
Mark <a0938765923@gmail.com>
2020-05-15 18:51:06 +0800

    add python

--- Project AT test/pythonLibrary.py
+++
@@ -1,9 +0,0 @@
-from robot.libraries.BuiltIn import BuiltIn
-from robot.api.deco import keyword
-
-class PythonTestLibrary:
-
-    @keyword(name='Set Throughput')
-    def set_throughput(self, _latency, _download_throughput, _upload_throughput):
-        selenium = BuiltIn().get_library_instance('SeleniumLibrary')
-        selenium.driver.set_network_conditions(latency=int(_latency), download_throughput=int(_download_throughput), upload_throughput=int(_upload_throughput))
